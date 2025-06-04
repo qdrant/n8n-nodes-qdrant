df6851dc4333733c4a3fcfa69d76acb5eed366c3
Anush008 <anushshetty90@gmail.com>
2025-06-04 17:36:37 +0530

    chore: Vector examples
    Signed-off-by: Anush008 <anushshetty90@gmail.com>

--- nodes/Qdrant/descriptions/UpsertPoints.ts
+++ nodes/Qdrant/descriptions/UpsertPoints.ts
@@ -27,7 +27,7 @@ export const upsertPointsFields: INodeProperties[] = [
 		displayName: 'Points',
 		name: 'points',
 		hint: 'List of points to upsert. Each point should have id, vector, and optional payload',
-		default: '[]',
+		default: '[\n    {\n        \"id\": 0,\n        \"payload\": {\n            \"color\": \"red\",\n            \"age\": 32\n        },\n        \"vector\": [\n            0.9,\n            0.1,\n            0.1\n        ]\n    },\n    {\n        \"id\": 1,\n        \"payload\": {\n            \"color\": \"green\",\n            \"age\": 55\n        },\n        \"vector\": [\n            0.1,\n            0.9,\n            0.1\n        ]\n    }\n]',
 		type: 'json',
 		required: true,
 		displayOptions: {
